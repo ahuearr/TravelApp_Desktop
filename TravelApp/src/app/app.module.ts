14075454a2bc5d65742232bfae030951a7b0b3e5
ahuearr <ahuearr@gmail.com>
2017-10-27 00:10:22 +0200

    Login Screen

--- TravelApp/src/app/app.module.ts
+++ TravelApp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { LoginModule } from './login/login.module';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const routes = [
+  {'path': 'login', component: LoginComponent},
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TravelApp/src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  title = 'Bienvenido a tu aplicacion para la gestion de viajes en grupo';
+}
