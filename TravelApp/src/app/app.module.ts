3107cefe61a8f8b2f7211339f7ddbd9cd91ed165
ahuearr <ahuearr@gmail.com>
2017-11-25 00:22:00 +0100

    Signup page

--- TravelApp/src/app/app.module.ts
+++ TravelApp/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ForgotComponent } from './login/forgot/forgot.component';
 
 
 
 const routes = [
   {'path': 'login', name: 'login', loadChildren: 'app/login/login/login.module#LoginModule'},
+  {'path': 'forgot', name: 'forgot', component: ForgotComponent},
+  {'path': 'signup', name: 'signup', loadChildren: 'app/login/signup/signup.module#SignupModule'},
   ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ForgotComponent,
   ],
   imports: [
     BrowserModule,
